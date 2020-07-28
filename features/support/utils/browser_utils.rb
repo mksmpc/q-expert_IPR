06c98dfe4d6cc6d8b342b14af01df4674289735c
Maksim Shvetsov <m.shvetsov@q-expert.com>
2020-07-28 09:00:51 +0300

    fix lfs

--- features/support/utils/browser_utils.rb
+++ features/support/utils/browser_utils.rb
@@ -2,16 +2,6 @@ def initialization_browser(scenario)
   @caps = Selenium::WebDriver::Remote::Capabilities.chrome(Settings.caps)
   @browser = Selenium::WebDriver.for(:chrome, desired_capabilities: @caps)
   @browser.manage.window.maximize
-  setup_browser_timeouts
-end
-
-
-def setup_browser_timeouts
-  timeout = Settings.timeouts
-  @browser.manage.timeouts.implicit_wait = timeout.implicit_wait
-  @browser.manage.timeouts.script_timeout = timeout.script_timeout
-  @browser.manage.timeouts.page_load = timeout.page_load
-  @wait = Selenium::WebDriver::Wait.new(timeout: timeout.page_load)
 end
 
 def screenshot
