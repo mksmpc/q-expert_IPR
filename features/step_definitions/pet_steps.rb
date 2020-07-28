9aaa32af869121245d9a3fe9f0774245c91552ab
Maksim Shvetsov <m.shvetsov@q-expert.com>
2020-07-28 05:57:15 +0300

    Создал тесты на удаление заказа

--- features/step_definitions/pet_steps.rb
+++ features/step_definitions/pet_steps.rb
@@ -3,3 +3,9 @@ When(/^Создали животное с id = (\d+)$/) do |id|
   @payload = JSON.generate({id: "#{id}"})
   step("Отправили POST на /pet")
 end
+
+When(/^Создали заказ с id = (\d+)$/) do |id|
+  @headers_hash.merge!({"Content-Type" => "application/json"})
+  @payload = JSON.generate({id: "#{id}"})
+  step("Отправили POST на /store/order")
+end
